4e173c6547f0d1d6eb901a0a66464eed495e0f18
p1kac1u <67856006+p1kac1u@users.noreply.github.com>
2021-01-01 13:10:21 +0600

    Update Quadratic_Equation.py

--- Quadratic_Equation.py
+++ Quadratic_Equation.py
@@ -9,7 +9,7 @@ c = Decimal(input("Enter the value of the coefficient c in your quadratic eqauti
 # Defining a function for calculating the roots
 def quadraticsolver(a,b,c):
     dis = (b ** 2) - 4 * a * c # Calculating the discriminant
-    sqrt_dis = math.sqrt(abs(dis))
+    sqrt_dis = Decimal(math.sqrt(abs(dis)))
     
     # Checking the condition of the discriminant
     if dis >= 0:
@@ -22,22 +22,37 @@ def quadraticsolver(a,b,c):
         root_2 = (-b - sqrt_dis) / (2 * a)
         print("The first root is", root_1, "i")
         print("The second root is", root_2, "i")
-    
+
+def moredecimal():
     # More Decimal digits
     print()
     print("Do you want more decimal digits of your root? (Y/N)")
     choice = input()
-    
+
     if choice == "Y":
+        dis = (b ** 2) - 4 * a * c # Calculating the discriminant
+        sqrt_dis = Decimal(math.sqrt(abs(dis)))
         print("How many digits of the root you want? ")
         nd = int(input())
         getcontext().prec = nd + 1
-        print(Decimal(root_1))
-        print(Decimal(root_2))
+        if dis >= 0:
+            root_1 = Decimal((-b + sqrt_dis) / (2 * a))
+            root_2 = Decimal((-b - sqrt_dis) / (2 * a))
+            print("The first root is", root_1)
+            print("The second root is", root_2)
+        elif dis < 0:
+            root_1 = Decimal((-b + sqrt_dis) / (2 * a))
+            root_2 = Decimal((-b - sqrt_dis) / (2 * a))
+            print("The first root is", root_1, "i")
+            print("The second root is", root_2, "i")
     elif choice == "N":
         print("Have a nice day. Goodbye")
-    elif choice == ""
+    elif choice != "Y" or "N":
+        print("Err! Your choice went over my head. Please try again.")
+        moredecimal()
+        
 if a == 0:
     print("The quadratic equation that you have entered is incorrect. Please try again.")
 else:
     quadraticsolver(a,b,c)
+    moredecimal()
